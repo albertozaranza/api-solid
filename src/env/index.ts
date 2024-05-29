3984adc78e47df63e25b2fe26444197cf9a8189b
Alberto Zaranza <albertozaranza@outlook.com>
2024-05-29 15:02:09 -0300

    setup project

---
+++ src/env/index.ts
@@ -0,0 +1,25 @@
+import { config } from "dotenv";
+
+if (process.env.NODE_ENV === "tst") {
+  config({ path: ".env.tst" });
+} else {
+  config();
+}
+
+import { z } from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "tst", "prd"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+  // DATABASE_URL: z.string(),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("Invalid environment variables!", _env.error.format());
+
+  throw new Error("Invalid environment variables!");
+}
+
+export const env = _env.data;
